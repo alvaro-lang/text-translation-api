8019ac32708a77c87d0417f1fba9a482dccef09a
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-03-09 22:13:19 +0000

    wsgi change settings

--- text_translation/text_translation/wsgi.py
+++ text_translation/text_translation/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text_translation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
